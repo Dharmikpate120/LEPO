ae88eb1b18887ad3e118f9a7e259b50d6ae8e426
Dharmikpate120 <dharmik7458@gmail.com>
2025-09-17 22:27:46 +0530

    initial commit

--- src/http/users.rs
+++ src/http/users.rs
@@ -4,7 +4,7 @@ use axum::routing::get;
 use crate::http::extractor::AuthUser;
 
 pub fn router() ->Router{
-    Router::new().route("/users", get(get_handler))
+    Router::new().route("/", get(get_handler))
 }
 
 async fn get_handler(auth_user:AuthUser) -> &'static str{
